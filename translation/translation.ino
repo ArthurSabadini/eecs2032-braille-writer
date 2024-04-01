9e9c40d3ed4855518b5b354a7b75bfe8af0adb15
Arthur Sabadini Nascimento <Arthur@MacBook-Air-de-Arthur.local>
2024-04-01 15:31:13 -0400

    Convertion Testing Fixes

--- translation/translation.ino
+++ translation/translation.ino
@@ -5,9 +5,12 @@
 #define DISPLAY_PIN 13 // LED for display
 #define POT_PIN A0 // Potentiometer pin
 
+// Shift Register Connections
 #define DS_PIN 4
 #define LATCH_PIN 5
 #define CLOCK_PIN 6
+
+// InfraRed Module Connection
 #define IR_PIN 2
 #define DELAY 250
 
@@ -47,23 +50,24 @@ IRdecoder irDecoder(IR_PIN);
 uint8_t symbol_buffer[3][2]{};
 byte serialDataFormat = B0;
 
-void convertToByte(uint8_t data[3][2], byte formattedByte) {
-    int index = 0;
-    formattedByte = B0;
+byte convertToByte(uint8_t data[3][2]) {
+    byte formattedByte = 0;
 
     // Convert data to byte format
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 2; j++) {
-            formattedByte = (formattedByte << 1) | (data[i][j] & 0x01);
+            formattedByte = (formattedByte << 1) | data[i][j];
         }
     }
 
-    // Add 0x00 at the end
-    formattedByte = (formattedByte << 1) | (0 & 0x01);
-    formattedByte = (formattedByte << 1) | (0 & 0x01);
+    // Add 0x00 at the end, since the shift register expects input to be 8-bits
+    formattedByte = (formattedByte << 1) | 0;
+    formattedByte = (formattedByte << 1) | 0;
+    return formattedByte;
 }
 
 void setup() {
+  Serial.begin(9600);
   // Read potentiometer value and map it to delay range (500 to 2000 ms)
   int delayValue = map(analogRead(POT_PIN), 0, 1023, 500, 2000);
 
@@ -71,46 +75,53 @@ void setup() {
   irDecoder.setup();
 
   // Initialize button pin
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  //pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   // Initialize potentiometer pin
-  pinMode(POT_PIN, INPUT);
+  //pinMode(POT_PIN, INPUT);
 
   // Initialize display pin
-  pinMode(DISPLAY_PIN, OUTPUT);
+  //pinMode(DISPLAY_PIN, OUTPUT);
 
   // Initialize shift register pins
   pinMode(DS_PIN, OUTPUT);
   pinMode(LATCH_PIN, OUTPUT);
   pinMode(CLOCK_PIN, OUTPUT);
+
+  irDecoder.beginReceiveInput();
 }
 
 void loop() {
-  // Check for IR input and process if available
-  if (irDecoder.isInInputMode) {
-    irDecoder.beginReceiveInput();
-  }
+    // Check for IR input and process if available
+    if(irDecoder.isInInputMode) {
+        String state = irDecoder.getStringfiedSymbol();
+        Serial.println(state);
+        //delay(DELAY);
+        return;
+    }
 
-  // Check if the button is pressed
-  if (digitalRead(BUTTON_PIN) == LOW) {
-      // Display and process the character
-      if (!irDecoder.input_buffer.empty()) {
-          // Display characters as the user goes through the incoming characters
-          while (!irDecoder.input_buffer.empty()) {
-              irDecoder.input_buffer.get(0, symbol_buffer);
-              convertToByte(symbol_buffer, serialDataFormat);
-              
-              displayCharacter(serialDataFormat);
-              delay(DELAY); // Display delay 
-              irDecoder.input_buffer.pop();
-          }
-      }
-  }
+    // Check if the button is pressed
+    //if (digitalRead(BUTTON_PIN) == LOW) {
+        // Display and process the character
+        if (!irDecoder.input_buffer.empty()) {
+            // Display characters as the user goes through the incoming characters
+            while (!irDecoder.input_buffer.empty()) {
+                irDecoder.input_buffer.get(irDecoder.input_buffer.size()-1, symbol_buffer);
+                serialDataFormat = convertToByte(symbol_buffer);
+
+                // Not actuating the solenoids, for testing
+                //displayCharacter(serialDataFormat);
+                Serial.println(serialDataFormat, HEX);
+                delay(DELAY); // Display delay 
+                irDecoder.input_buffer.pop();
+            }
+        }
+        exit(0);
+    //}
 }
 
 // Function to display a character on the display
 void displayCharacter(byte serialDataFormat) {
-  //const char* braillePattern = braille[brailleIndex];
   // Send the solenoid pattern via shift register
   digitalWrite(LATCH_PIN, LOW);
   shiftOut(DS_PIN, CLOCK_PIN, LSBFIRST, serialDataFormat); // Only sending the first byte
